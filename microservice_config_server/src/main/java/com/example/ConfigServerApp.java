a1611accc52154f3daa989a90311370e096725f8
Rafael Blanco Cordero <rafabc1979@gmail.com>
2016-12-06 18:00:31 +0100

    config changes

--- microservice_config_server/src/main/java/com/example/ConfigServerApp.java
+++ microservice_config_server/src/main/java/com/example/ConfigServerApp.java
@@ -1,19 +1,15 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableConfigServer
 @EnableEurekaClient
-@RefreshScope
 public class ConfigServerApp {
 
 	public static void main(String[] args) {
